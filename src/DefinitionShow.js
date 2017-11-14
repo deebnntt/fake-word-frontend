468bb262d49cadfc3f8569ad7de1987656c223c8
deebnntt <daniellebennettchan@gmail.com>
2017-11-14 10:11:37 -0500

    added fetch to definition show page

--- src/DefinitionShow.js
+++ src/DefinitionShow.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
 export default class DefinitionShow extends React.Component {
-	componentDidMount() {}
+	state = {
+		definition: { word: {} }
+	};
+
+	componentDidMount() {
+		const id = this.props.match.params.id;
+		fetch(`http://localhost:3000/api/v1/definitions/${id}`)
+			.then(res => res.json())
+			.then(json => {
+				this.setState(
+					{
+						definition: json
+					},
+					() => {
+						console.log(
+							'this',
+							this,
+							'this.state',
+							this.state,
+							'this.state.definition',
+							this.state.definition
+						);
+					}
+				);
+			});
+	}
 
 	displayDefinitions = () => {
-		if (this.props.definition) {
-			console.log(this.props.definition);
+		if (this.state.definition) {
 			return (
 				<div>
 					<ul>
-						<h3>{this.props.definition.word.word}</h3>
-						<li>Part of Speech: {this.props.definition.part_of_speech}</li>
-						<li>Definition: {this.props.definition.definition_text}</li>
-						<li>Sentence: {this.props.definition.sentence}</li>
+						<h3>{this.state.definition.word.word}</h3>
+						<li>Part of Speech: {this.state.definition.part_of_speech}</li>
+						<li>Definition: {this.state.definition.definition_text}</li>
+						<li>Sentence: {this.state.definition.sentence}</li>
 					</ul>
 				</div>
 			);
